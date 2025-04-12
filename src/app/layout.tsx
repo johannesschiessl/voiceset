c1deedaa960d987ce0417d66c6a05b14d8025a65
Johannes Schießl <ignotumnomen@icloud.com>
2025-04-12 11:26:22 +0200

    fix(app): update metadata import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
+import Metadata from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { FeedbackButton } from "@/components/feedback-button";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
